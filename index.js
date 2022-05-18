697060ba926d6f2052b0c25f7d892f5cd6c239a0
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-05-18 15:06:27 +0200

    done backend with Quiz Schema

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+
+const quizSchema = new mongoose.Schema({
+  antwort1: String,
+  antwort2: String,
+  antwort3: String,
+});
+
+const Quiz = mongoose.model("Quiz", quizSchema, "quiz");
+
+app.use("/", async (req, res) => {
+  try {
+    const quizes = await Quiz.find().exec();
+    res.json(quizes);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+mongoose.connect(process.env.MONGO_CONECTION).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Quiz API is listening on port http://localhost:${PORT}`);
+  });
+});
